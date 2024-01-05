8d46f9579b61fc93aac8975ccd92bb05f3dec014
Muhammad Awais <muhammad.awais@systempavers.com>
2024-01-05 16:19:36 +0500

    added some variables in scss along with lazy laoding approach for rendering components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: ()=> import('./pages/my-reports/my-reports.component').then(c => c.MyReportsComponent)
+    },
+    {
+        path: 'my-reports',
+        loadComponent: ()=> import('./pages/my-reports/my-reports.component').then(c => c.MyReportsComponent)
+    },
+    {
+        path: 'ai-automation',
+        loadComponent: ()=> import('./pages/ai-automations/ai-automations.component').then(c => c.AiAutomationsComponent)
+    }
+];
