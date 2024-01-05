87e0c7a6b8d18628ef5920ce7b1a2d4697c71c67
Muhammad Awais <muhammad.awais@systempavers.com>
2024-01-05 17:30:07 +0500

    sidebar done UI

---
+++ src/app/interfaces/route.interface.ts
@@ -0,0 +1,5 @@
+export interface Route {
+    path: string
+    icon: string
+    name: string
+}
--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ROUTES } from '../../routes';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Route } from '../../interfaces/route.interface';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,9 +10,10 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
+
 export class SidebarComponent {
 
-  routes: any = [
+  routes: Route[] = [
     {
       path: ROUTES.MY_REPORTS,
       icon: 'bi bi-speedometer',
@@ -26,6 +28,45 @@ export class SidebarComponent {
       path: ROUTES.AI_AUTOMATIONS,
       icon: 'bi bi-wechat',
       name: 'AI & Automations'
+    }
+  ]
+
+  yesterdayRoutes: Route[] = [
+    {
+      path: 'sell-goods',
+      icon: '',
+      name: 'Sale of goods'
+    },
+    {
+      path: 'audience',
+      icon: '',
+      name: 'Audience'
+    },
+    {
+      path: 'my-sells',
+      icon: '',
+      name: 'My Sells'
+    }
+  ]
+
+  previousSevenRoutes: Route[] = [
+    {
+      path: 'sell-week',
+      icon: '',
+      name: 'Sell of week'
+    },
+    {
+      path: 'sell-month',
+      icon: '',
+      name: 'Sell 12$ per month'
+    }
+  ]
+
+  previousThirtyRoutes: Route[] = [
+    {
+      path: 'help-business',
+      icon: '',
+      name: 'Help for business'
     }
   ]
 
